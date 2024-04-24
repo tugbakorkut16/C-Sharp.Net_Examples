f1cadda01928e26ddbaa389f4860b181060b9d2f
tugbakorkut16 <tugbakorkut16@gmail.com>
2024-04-25 01:17:36 +0300

    dört_islem2 added

---
+++ Dört_islem_2.cs
@@ -0,0 +1,45 @@
+﻿// kullanıcıya sunulan bir menü içinden seçilecek olan dört işlem tipine göre hesaplama yapınız.
+
+
+Console.Write("Birinci sayi:");
+var sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("ikinci sayi:");
+var sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Toplama için: +");
+Console.WriteLine("Çikarma için: -");
+Console.WriteLine("Çarpma için : *");
+Console.WriteLine("Bölme için  : /");
+
+Console.Write("Seçiminiz: ");
+var secim = Console.ReadLine();
+
+double sonuc = 0;
+bool ok = true;
+
+if (secim == "+") {
+   //toplama
+   sonuc = sayi1 + sayi2;
+} else if (secim =="-") {
+   //çıkarma
+   sonuc = sayi1 - sayi2;  
+} else if (secim =="*") {
+   //çarpma
+   sonuc = sayi1 * sayi2;
+} else if (secim =="/") {
+   //bölme
+   sonuc = sayi1 / sayi2;  
+} else {
+   //yanlış seçim,
+   ok = false;
+   Console.WriteLine("yanliş seçim tekrar deneyin");
+}
+if (ok){
+Console.WriteLine($"{sayi1} {secim} {sayi2} = {sonuc}");
+}
+
+
+
+
+
